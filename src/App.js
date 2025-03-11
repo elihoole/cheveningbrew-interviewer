e51fa932cc57a1e3074aeb26f96c16fdad4af5ae
elihoole <elihoole@gmail.com>
2025-03-11 13:12:08 +0530

    Google Auth configured

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import LandingPage from "./pages/Landing/LandingPage";
 import Upload from "./pages/Upload/Upload";
-import Interview from "./pages/Interview/Interview"; 
-import Feedback from "./pages/Feedback/Feedback"; 
-import Help from "./pages/SupportPages/Help/Help";  
-import About from "./pages/SupportPages/About/About"; 
-import Pricing from "./pages/SupportPages/Pricing/Pricing"; 
-import Privacy from "./pages/SupportPages/Privacy/Privacy"; 
-import Terms from "./pages/SupportPages/Terms/Terms"; 
-
+import Interview from "./pages/Interview/Interview";
+import Feedback from "./pages/Feedback/Feedback";
+import Help from "./pages/SupportPages/Help/Help";
+import About from "./pages/SupportPages/About/About";
+import Pricing from "./pages/SupportPages/Pricing/Pricing";
+import Privacy from "./pages/SupportPages/Privacy/Privacy";
+import Terms from "./pages/SupportPages/Terms/Terms";
 
 function App() {
   return (
@@ -19,14 +18,14 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/upload" element={<Upload />} />
-          <Route path="/interview" element={<Interview />} /> 
+          <Route path="/interview" element={<Interview />} />
           <Route path="/feedback" element={<Feedback />} />
-          <Route path="/help" element={<Help />} /> 
-          <Route path="/about" element={<About />} /> 
-          <Route path="/pricing" element={<Pricing />} /> 
-          <Route path="/privacy" element={<Privacy />} /> 
-          <Route path="/terms" element={<Terms />} /> 
-          
+          <Route path="/help" element={<Help />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+
           {/* Add other routes */}
         </Routes>
       </BrowserRouter>
